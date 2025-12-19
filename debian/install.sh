a89709f50c248fbb0bd32d2a9e7fd2780b6065f6
Dave Franco <davefranco1987@gmail.com>
2025-12-19 10:57:38 +0100

    check installation

--- debian/install.sh
+++ debian/install.sh
@@ -213,6 +213,12 @@ install_neovim() {
 install_awscli() {
   print_info "Installing aws-cli"
 
+  if command -v aws &>/dev/null; then
+    print_warning "aws-cli is already installed"
+    aws --version | head -n 1
+    return 0
+  fi
+  
   # Download install package
   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 
